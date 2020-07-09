8be278860399ca29f525929e518c60a0773c7a71
root <root@LAPTOP-35UT47I6.localdomain>
2020-07-08 22:16:59 -0300

    new landing page

--- js/aux.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('tags')
-  .addEventListener('change', function(el){
-    var value = this.options[this.selectedIndex].value;
-    var txt = location.origin;
-    if(value !== '-'){
-      txt = txt.concat('/tags/').concat(value)
-    }
-    location.replace(txt)
-  })
